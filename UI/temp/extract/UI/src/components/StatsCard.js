2ee6b3bf189c5f752f3c134be8f462cb46c6a035
LevviCodeID <levvicode@gmail.com>
2026-03-13 12:12:02 +0700

    Delete UI/temp/extract/UI directory

--- UI/temp/extract/UI/src/components/StatsCard.js
+++
@@ -1,9 +0,0 @@
-export default function StatsCard({ icon, title, value, color }) {
-  return (
-    <div className="stats-card">
-      <i className={`bi ${icon}`} style={{ color, fontSize: '2rem' }}></i>
-      <h3>{title}</h3>
-      <div className="value">{value}</div>
-    </div>
-  );
-}
