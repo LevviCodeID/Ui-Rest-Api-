5ff53325d69a222bdac1ce278a57d8bb74319823
LevviCodeID <levvicode@gmail.com>
2026-03-13 08:52:35 +0700

    Add files via upload

--- UI/temp/extract/UI/src/services/api.js
+++ UI/temp/extract/UI/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = process.env.REACT_APP_API_BASE || 'https://api.levvicode.cloud';
+const API_BASE = 'https://api.levvicode.cloud';
 
 const api = axios.create({ baseURL: API_BASE });
 
